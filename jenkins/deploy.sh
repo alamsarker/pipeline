de610fd402d4bba8a09cbd45bdc18236d4a3a45e
Md. Kausar Alam <alam.iiuc@gmail.com>
2020-11-10 18:52:47 +0600

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,6 +6,6 @@ PROJECT_NAME="isalebd"
 BUILD_TAR_FILE="$PROJECT_NAME.tar.gz"
 REMOTE_SERVER="root@104.131.57.194"
 
-APP_DIR="/var/www/html/${ENV_SERVER}/$PROJECT_NAME";
+APP_DIR="/var/www/html/$ENV_SERVER/$PROJECT_NAME";
 
 echo $APP_DIR
