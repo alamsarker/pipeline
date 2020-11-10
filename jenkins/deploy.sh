19e2b8aeedaf704f5d20eae2f20ae6e580f24f0e
Md. Kausar Alam <alam.iiuc@gmail.com>
2020-11-10 18:43:50 +0600

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -8,42 +8,4 @@ REMOTE_SERVER="root@104.131.57.194"
 
 APP_DIR="/var/www/html/${ENV_SERVER}/$PROJECT_NAME";
 
-excludeDirs=(
-    "./.git"
-    "./data"
-    "./."
-    "./.."
-    "./app/cache"
-    "./app/logs"
-    "./composer.phar"
-    "./phpunit.phar"
-    "./node_modules"
-    "./bower_components"
-    "./.vscode"
-    "./package-lock.json"
-    "./build.tar.gz"
-    "./web/js/app/ng",
-    "./web/css/app/less"
-)
-
-tarCommand="tar "
-
-for dir in "${excludeDirs[@]}"
-do
-    tarCommand+="--exclude=$dir "
-done
-
-tarCommand+="-czf $BUILD_TAR_FILE ./"
-
-rm -rf $BUILD_TAR_FILE
-$tarCommand
- 
-scp $BUILD_TAR_FILE $REMOTE_SERVER:$APP_DIR/
-
-ssh -T $REMOTE_SERVER << EOF
-    cd $APP_DIR
-    pwd
-    tar -xvf $BUILD_TAR_FILE
-    rm -rf app/cache/*
-    rm -rf app/logs/*
-EOF
+echo $APP_DIR
