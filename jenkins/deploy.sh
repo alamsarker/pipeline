c7bb32c1c4a45e48c6d056abe439d425ffe81f8f
Md. Kausar Alam <alam.iiuc@gmail.com>
2020-11-11 13:03:21 +0600

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -8,6 +8,11 @@ PROJECT_NAME="isalebd"
 BUILD_TAR_FILE="$PROJECT_NAME.tar.gz"
 REMOTE_SERVER="root@104.131.57.194"
 
-APP_DIR="/var/www/html/$ENV_SERVER/$PROJECT_NAME";
+if [[ -z $ENV_SERVER ]]
+then
+     echo "Empty params given."
+     exit  
+fi
 
+APP_DIR="/var/www/html/$ENV_SERVER/$PROJECT_NAME";
 echo $APP_DIR
