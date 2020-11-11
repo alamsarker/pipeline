2a7a88ed9f6bf485f48a3e851ee240a459e28188
Md. Kausar Alam <alam.iiuc@gmail.com>
2020-11-11 12:30:11 +0600

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+export ENV_SERVER=$1
+
 echo 'Deploying...'
 
 PROJECT_NAME="isalebd"
 BUILD_TAR_FILE="$PROJECT_NAME.tar.gz"
 REMOTE_SERVER="root@104.131.57.194"
-ENV_SERVER="${ENV_SERVER}"
 
 APP_DIR="/var/www/html/$ENV_SERVER/$PROJECT_NAME";
 
